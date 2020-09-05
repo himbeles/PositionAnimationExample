6f1f5b8e0805fae009051e9297a1b39bdf0644c7
Luis Riegger <lri@me.com>
2020-09-05 20:17:28 +0200

    Initial Commit

---
+++ Shared/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  Shared
+//
+//  Created by Luis on 05.09.20.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
