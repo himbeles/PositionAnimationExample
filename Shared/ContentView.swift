76465ccfc0825d8ff7eb0c5cc19c9b5c960c1843
Luis Riegger <lri@me.com>
2020-09-07 08:21:52 +0200

    initial commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,16 +1,23 @@
-//
-//  ContentView.swift
-//  Shared
-//
-//  Created by Luis on 05.09.20.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State var toggle = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            if self.toggle {
+                Rectangle().frame(width: 200, height: 200)
+            }
+            Button(action: {
+                self.toggle.toggle()
+            }){
+                Text("Tap me!")
+                    .scaleEffect(self.toggle ? 2 : 1)
+                    .animation(
+                        Animation.easeInOut(duration: 1.5)
+                    )
+            }
+        }
+        .frame(width: 400, height: 400)
     }
 }
 
